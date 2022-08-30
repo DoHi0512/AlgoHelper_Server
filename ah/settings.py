304d817aded85d378ec8f938acd782c694abe90a
DoHi0512 <donghuni512@gmail.com>
2022-08-30 19:46:43 +0900

    스케쥴러

--- ah/settings.py
+++ ah/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'allauth.socialaccount',
     'ahapp',
+    'django_apscheduler',
 ]
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"  # Default
+
+SCHEDULER_DEFAULT = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
